1d0a8a83baf3c3ae90bd51555414ac86ae02ff40
Daniel Mai <danielmai1728@gmail.com>
2025-12-24 10:39:00 -0800

    connect to dynamodb with a POST endpoint

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,19 @@
+import { table } from "./storage";
 import { bucket } from "./storage";
 
-export const api = new sst.aws.ApiGatewayV2("Api");
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      }
+    }
+  },
+});
 
 api.route("GET /", {
   link: [bucket],
   handler: "packages/functions/src/api.handler",
 });
+
+api.route("POST /notes", "packages/functions/src/create.main")
